dbb03fef0cf566c95932f3930b609475f86aae8c
noBthd <Egorkir0401@gmail.com>
2025-05-15 11:57:19 +0300

    added get all users services

--- internal/handlers/user_handler.go
+++ internal/handlers/user_handler.go
@@ -4,7 +4,7 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"github.com/yourname/5x30-fitness-api/internal/services"
+	"github.com/noBthd/5x30-fitness-api/internal/services"
 )
 
 func GetUsersHandler(c *gin.Context) {
