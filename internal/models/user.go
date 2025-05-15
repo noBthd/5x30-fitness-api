93192e9c9cc5b80b403a1316235140de417c2003
noBthd <Egorkir0401@gmail.com>
2025-05-15 12:28:15 +0300

    added right names to json

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,10 +1,10 @@
 package models
 
 type User struct {
-	ID		 string
-	Username string 
-	Email	 string
-	Password string
-	RegDate  string
+	ID		 string `json:"id"`
+	Username string `json:"username"`
+	Email	 string `json:"email"`
+	Password string `json:"password"`
+	RegDate  string `json:"registration_date"`
 }
 
